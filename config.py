8f58a3150ca8b16cf04532c32b32e2a235abc643
ishowlab-boop <ishowlab@gmail.com>
2026-02-02 01:24:25 +0600

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,11 @@ ADMIN_IDS = [OWNER_ID] if OWNER_ID else []
 
 DB_PATH = os.getenv("DB_PATH", "file.db")
 
+# credits (only for VIDEO)
 FREE_CREDITS = int(os.getenv("FREE_CREDITS", "2"))
 CREDITS_PER_VIDEO = int(os.getenv("CREDITS_PER_VIDEO", "1"))
 
+# links / channel
 REQUIRED_CHANNEL = os.getenv("REQUIRED_CHANNEL", "@iuo82828")
 VOICE_SUPPORT_LINK = os.getenv("VOICE_SUPPORT_LINK", "https://t.me/ariyanvoice")
 MODEL_SUPPORT_LINK = os.getenv("MODEL_SUPPORT_LINK", "https://modelboxbd.com")
